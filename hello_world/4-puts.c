871b4af62e0ccaf796c2751f22be2677c556cf16
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-10-12 13:46:12 -0400

    puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /*
- * main prints "Programming is like building a multilingual puzzle
- * return (0)
+ * main -  prints Programming is like building a multilingual puzzle
+ * Return: (0)
 */
 
 
