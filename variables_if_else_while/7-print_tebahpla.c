8581c9eb317a541b62145fb0fd5ebdb0027fdc76
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-10-16 15:38:05 -0400

    putcha

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ *
+ */
+
 int main(void)
 {
-    char letter;
-
-    for (letter = 'z'; letter >= 'a'; letter--)
-    {
-        putchar(letter);
-    }
-
-    putchar('\n');
-
-    return 0;
+	char letter;
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return 0;
 }
-
