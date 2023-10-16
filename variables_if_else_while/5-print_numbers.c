65df695ea2d9b9c039815eab88586099f16e621d
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-10-16 15:00:37 -0400

    putcha

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    int number = 0;
+	int num;
 
-    while (number <= 9)
-    {
-        printf("%d\n", number, number++);
-    }
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
 
-    return 0;
+	printf("\n");
+
+	return (0);
 }
-
