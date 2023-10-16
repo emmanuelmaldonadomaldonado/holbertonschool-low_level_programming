debd49b60f349b0c8c5384689e6aee310289f477
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-10-16 14:20:33 -0400

    putcharbut no q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char alphabet;
+
+    for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+    {
+        if (alphabet != 'q' && alphabet != 'e')
+        {
+            putchar(alphabet);
+        }
+    }
+
+    putchar('\n');
+    return 0;
+}
