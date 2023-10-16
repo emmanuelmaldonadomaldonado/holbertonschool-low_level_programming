85787984e942672fbd8bec022e149c0775a576dc
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-10-16 15:30:07 -0400

    putcha

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ *
+ */
+
+
+int main() {
+
+	int c;
+	for (c = 'z'; c >= 'a'; --c) {
+		printf("%c ", c);
+	}
+	printf("\n");
+
+}
