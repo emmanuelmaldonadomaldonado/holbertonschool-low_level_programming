49b42bb731600fc403cf76df4bb4deba724739d8
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-10-16 14:55:41 -0400

    putcha

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
 
-/**
- * main - main block
- * Description: Print all possible combinations of single-digit numbers.
- * Numbers must be separated by commas and a space.
- * You can only use `putchar` to print to the console.
- * You can only use `putchar` up to four times.
- * You are not allowed to use any variable of type `char`.
- * Return: 0
- */
-
 int main(void)
 {
-    int x;
+    int number = 0;
 
-    for (x = 48; x < 58; x++)
+    while (number <= 9)
     {
-        putchar(x);
-        if (x < 57)
-        {
-            putchar(44);
-            putchar(32);
-        }
+        printf("%d\n", number);
+        number++;
     }
-    putchar('\n');
-    return (0);
+
+    return 0;
 }
+
