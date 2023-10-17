0efbfb7db436331dd1e18ff06b2b169276c587dc
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-10-17 14:07:45 -0400

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *@c: check 
+ *
+ * Return: 1 if lower, 0 if not
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
