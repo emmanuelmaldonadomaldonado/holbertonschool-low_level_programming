92f8d13a82370b0375a5213c7963cfcc2d72d020
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-10-19 13:22:03 -0400

    mul

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,13 +1,12 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
- * mul - Entry point
- * @c: character to verify
- *
- * Return: a * b
- */
-
-int mul(int a, int b) 
+* mul - multiplies two integers
+* @a: first integer
+* @b: second integer
+* Return: addition of a and b
+*/
+int mul(int a, int b)
 {
 	return (a * b);
 }
