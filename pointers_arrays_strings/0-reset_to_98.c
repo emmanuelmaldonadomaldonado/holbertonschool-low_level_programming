b02744e37ab480734056d94016297979304d0947
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-10-23 11:26:17 -0400

    ponter

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,14 @@
-#include "main.h"
-#include <stdio.h>
+#include <stdio>
 
 /**
- * main - check the code
+ * reset_to_98 - Takes a pointer to an int and updates
+ * the value it points points to 98
  *
- * Return: Always 0.
+ * @n: value to check
+ *
+ * Return: nothing
  */
-int main(void)
+void reset_to_98(int *n)
 {
-	int n;
-
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
+	*n = 98;
 }
