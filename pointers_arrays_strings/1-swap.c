29d1956da67645c347cc73dcaaae0a124cc257e7
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-10-23 12:46:36 -0400

    a b

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values
+ * @a: The fist integer
+ * @b: the second integer
+ *
+ */
+
+void swap_int(int *a, int *b);
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
