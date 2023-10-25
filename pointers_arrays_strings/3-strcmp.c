236d6df8ce5be26b93b96c413ed7b65c08e42146
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-10-25 11:32:36 -0400

    scp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -6,19 +6,19 @@
  * @s2: The second string to compare.
  *
  * Return: An integer less than, equal to, or greater than zero
- *         if s1 is found to be less than, equal to, or greater than s2, respectively.
+ * if s1 is found to be
+ * less than, equal to, or greater than s2, respectively.
  */
 int _strcmp(char *s1, char *s2)
 {
-    while (*s1 != '\0' && *s2 != '\0')
-    {
-        if (*s1 != *s2)
-        {
-            return (*s1 - *s2);
-        }
-        s1++;
-        s2++;
-    }
-
-    return (*s1 - *s2);
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
