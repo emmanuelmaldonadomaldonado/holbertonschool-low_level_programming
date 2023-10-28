e4d575d8dd28c46366c5a5c02ae5c1ed35ace894
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-10-28 14:46:23 -0400

    factorial

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -11,12 +11,12 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 	{
-		return -1;  /* Return -1 to indicate an error if y is less than 0. */
+		return (-1);
 	}
 	if (y == 0)
 	{
-		return 1;   /* Return 1 if y is 0 (base case: x^0 = 1). */
+		return (1);
 	}
-	return x * _pow_recursion(x, y - 1);  /* Recursive call for x^y = x * x^(y-1). */
+	return (x * _pow_recursion(x, y - 1));
 }
 
