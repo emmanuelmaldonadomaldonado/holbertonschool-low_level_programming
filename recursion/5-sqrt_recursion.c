2e8b9d04bc69eae5c84c81c2db351ec44d266512
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-10-28 16:22:14 -0400

    sqrt

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,21 +1,31 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Calculates the natural square root of a number.
- * @n: The number for which to find the square root.
+ * prime2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
  *
- * Return: The square root of n, or -1 if n is negative
- * or does not have a natural square root.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int _sqrt_recursion(int n)
+int prime2(int a, int b)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-	return (sqrt_helper(n, 1));
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime2(a, b + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime2(n, 2));
 }
