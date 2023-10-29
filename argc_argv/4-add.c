deb230a7bd195daaa0fef7eb166bcf10ff08471f
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-10-29 18:10:54 -0400

    yes

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,36 +1,44 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
 /**
- * main - Entry point
- * Description: Add positive numbers and print the result.
- * @argc: The number of command-line arguments
- * @argv: An array of strings containing the command-line arguments
- * Return: 0 for success, 1 for error
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
  */
+
 int main(int argc, char *argv[])
 {
-	int sum = 0;
+	int i, j, length, sum;
+	char *ptr;
 
-	if (argc == 1)
-	{
+	if (argc < 2)
 		printf("0\n");
-		return (0);
-	}
-	for (int i = 1; i < argc; i++)
+	else
 	{
-		for (int j = 0; argv[i][j] != '\0'; j++)
+		sum = 0;
+		for (i = 1; i < argc; i++)
 		{
-			if (!isdigit(argv[i][j]))
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
 			{
-				printf("Error\n");
-				return (1);
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
+
+			sum += atoi(argv[i]);
 		}
-		sum += atoi(argv[i]);
-	}
+
 	printf("%d\n", sum);
+	}
 	return (0);
 }
