cef5619f8485d7a5a6c9dffa1cdee606d772f290
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-11-11 15:31:59 -0400

    typedef

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,35 +1,42 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * new_dog - entry point
- * @name: string from main, name of pet
- * @age: number from main, age of pet
- * @owner: string from main, owner of pet
- * Return: p
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: Pointer to the newly created dog_t, or NULL if fails.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
-	/* reserving memory to struct*/
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
-		return (NULL);
-	/* Cpunting name pointer*/
-	if (name == NULL)
-	{
-		free(p);
-		free(owner);
-		return (NULL);
-	}
-	if (owner == NULL)
-	{
-		free(p);
-		free(name);
-		return (NULL);
-	}
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
+    dog_t *p;
+
+    /* Reserving memory for struct */
+    p = malloc(sizeof(dog_t));
+    if (p == NULL)
+        return (NULL);
+
+    /* Allocating memory for name and copying content */
+    p->name = strdup(name);
+    if (p->name == NULL)
+    {
+        free(p);
+        return (NULL);
+    }
+
+    /* Allocating memory for owner and copying content */
+    p->owner = strdup(owner);
+    if (p->owner == NULL)
+    {
+        free(p->name);
+        free(p);
+        return (NULL);
+    }
+
+    p->age = age;
+
+    return (p);
 }
