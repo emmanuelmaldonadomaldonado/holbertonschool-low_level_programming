04f00ff2425e3af1e70c1ce46101bb7c0961f857
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-11-12 18:28:15 -0400

    yay

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -10,23 +10,21 @@
  */
 int (*get_op_func(char *s __attribute__((unused))))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    int i = 0;
-
-    while (ops[i].op != NULL)
-    {
-        if (*(ops[i].op) == *s && s[1] == '\0')
-            return ops[i].f;
-        i++;
-    }
-
-    return NULL; /* Return NULL if the operator is not recognized */
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s && s[1] == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL); /* Return NULL if the operator is not recognized */
 }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,30 +2,38 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ * Return: 0 on success, other values on failure
+ */
+
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*operation)(int, int);
+	int num1, num2, result;
+	int (*operation)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        return 98;
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operation = get_op_func(argv[2]);
 
-    operation = get_op_func(argv[2]);
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		return (99);
 
-    if (operation == NULL)
-    {
-        printf("Error\n");
-        return 99;
-    }
+		result = operation(num1, num2);
 
-    result = operation(num1, num2);
-    printf("%d\n", result);
+		printf("%d\n", result);
 
-    return 0;
+		return (0);
+	}
 }
