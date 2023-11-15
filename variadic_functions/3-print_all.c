6640747bd34f608ee9c592b7ccda35147cf823a6
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-11-15 10:11:01 -0400

    yay

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,83 +1,53 @@
-#include <stdio.h>
 #include <stdarg.h>
-#include <string.h>
-#include "variadic_functions.h"
-/**
- * print_c - prints chars
- * @my_args: pointer to args
- */
-void print_c(va_list *my_args)
-{
-	printf("%c", va_arg(*my_args, int));
-}
-/**
- * print_i - prints ints
- * @my_args: pointer to args
- */
-void print_i(va_list *my_args)
-{
-	printf("%d", va_arg(*my_args, int));
-}
-/**
- * print_f - prints floats
- * @my_args: pointer to args
- */
-void print_f(va_list *my_args)
-{
-	printf("%f", va_arg(*my_args, double));
-}
-/**
- * print_s - prints strings
- * @my_args: pointer to args
- */
-void print_s(va_list *my_args)
-{
-	char *current_str;
+#include <stdio.h>
 
-	current_str = va_arg(*my_args, char *);
-	if (current_str == NULL)
-		current_str = "(nil)";
-	printf("%s", current_str);
-}
 /**
- * print_all - prints everything
- * @format: constant format string
- *
- * Return: void
+ * print_all - Print anything.
+ * @format: A list of types of arguments passed to the function.
+ * @...: The variable number of arguments to be printed.
  */
+void print_all(const char * const format, ...) {
+	va_list args;
+	unsigned int i = 0;
+	char *separator = "";
+	const char *cur_fmt;
 
-void print_all(const char * const format, ...)
-{
-	int i = 0, j = 0;
-	va_list my_args;
-	char *separator = ""; /*Prnt before prnts from functions,except last one */
-	data my_data[] = {
-		{"c", print_c},
-		{"i", print_i},
-		{"f", print_f},
-		{"s", print_s},
-		{NULL, NULL} /*letter-type, (function*) */
-	};/*Struct char *type, void (*f)()*/
+	va_start(args, format);
 
-	va_start(my_args, format);
+	while (format != NULL && format[i] != '\0') {
+		cur_fmt = &format[i];
 
-	while (format != NULL && format[i] != '\0')/*lettrs exist in format,not null*/
-	{
-		j = 0;
-		while (my_data[j].f != NULL)/*still have functions to call*/
-		{
-			if (*my_data[j].type == format[i])/*match letters = data type */
-			{
-				printf("%s", separator);/*prnts before function, last print has no sep*/
-				my_data[j].f(my_args);/*call matching print function */
-				separator = ", ";/*set value after 1st print*/
-				break;
-			}
-			j++;
+		switch (cur_fmt[0]) {
+		case 'c': {
+			printf("%s%c", separator, va_arg(args, int));
+			break;
 		}
+		case 'i': {
+			printf("%s%d", separator, va_arg(args, int));
+			break;
+		}
+		case 'f': {
+			printf("%s%f", separator, va_arg(args, double));
+			break;
+		}
+		case 's': {
+			char *str = va_arg(args, char *);
+			if (str == NULL) {
+				printf("%s(nil)", separator);
+			} else {
+				printf("%s%s", separator, str);
+			}
+			break;
+		}
+		default:
+			i++;
+			continue;
+		}
+
+		separator = ", ";
 		i++;
 	}
 
-	va_end(my_args);
 	printf("\n");
+	va_end(args);
 }
