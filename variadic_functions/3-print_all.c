d783776f17f8fc41d0c89996010793ef60642fc6
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-11-15 09:50:21 -0400

    yay

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,52 +1,96 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
- * print_all - Print anything.
- * @format: A list of types of arguments passed to the function.
- * @...: The variable number of arguments to be printed.
+ * printf_char - printfs a char from var args
+ *
+ * @list: va_list to print from
+ *
+ * Return: void
  */
-void print_all(const char * const format, ...) {
-    va_list args;
-    unsigned int i = 0;
-    char *separator = "";
-    const char *cur_fmt;
+void printf_char(va_list list)
+{
+	printf("%c", (char) va_arg(list, int));
+}
 
-    va_start(args, format);
+/**
+ * printf_int - printfs an int from var args
+ *
+ * @list: va_list to print from
+ *
+ * Return: void
+ */
+void printf_int(va_list list)
+{
+	printf("%d", va_arg(list, int));
+}
 
-    while (format != NULL && format[i] != '\0') {
-        cur_fmt = &format[i];
+/**
+ * printf_float - printfs a float from var args
+ *
+ * @list: va_list to print from
+ *
+ * Return: void
+ */
+void printf_float(va_list list)
+{
+	printf("%f", (float) va_arg(list, double));
+}
 
-        switch (cur_fmt[0]) {
-        case 'c':
-            printf("%s%c", separator, va_arg(args, int));
-            break;
-        case 'i':
-            printf("%s%d", separator, va_arg(args, int));
-            break;
-        case 'f':
-            printf("%s%f", separator, va_arg(args, double));
-            break;
-        case 's':
-            {
-                char *str = va_arg(args, char *);
-                if (str == NULL) {
-                    printf("%s(nil)", separator);
-                } else {
-                    printf("%s%s", separator, str);
-                }
-            }
-            break;
-        default:
-            i++;
-            continue;
-        }
+/**
+ * printf_string - printfs a string from var args
+ *
+ * @list: va_list to print from
+ *
+ * Return: void
+ */
+void printf_string(va_list list)
+{
+	char *str = va_arg(list, char*);
 
-        separator = ", ";
-        i++;
-    }
+	while (str != NULL)
+	{
+		printf("%s", str);
+		return;
+	}
+	printf("(nil)");
+}
 
-    printf("\n");
-    va_end(args);
+
+/**
+ * print_all - prints various types given a format string for the arguments
+ *
+ * @format: string containing type information for args
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	const char *ptr;
+	va_list list;
+	funckey key[4] = { {printf_char, 'c'}, {printf_int, 'i'},
+			   {printf_float, 'f'}, {printf_string, 's'} };
+	int keyind = 0, notfirst = 0;
+
+	ptr = format;
+	va_start(list, format);
+	while (format != NULL && *ptr)
+	{
+		if (key[keyind].spec == *ptr)
+		{
+			if (notfirst)
+				printf(", ");
+			notfirst = 1;
+			key[keyind].f(list);
+			ptr++;
+			keyind = -1;
+		}
+		keyind++;
+		ptr += keyind / 4;
+		keyind %= 4;
+	}
+	printf("\n");
+
+	va_end(list);
 }
