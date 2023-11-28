cfd2b996611d84394b3eadcd3fb4f2b8b6481348
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-11-27 22:29:03 -0400

    lists

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the beginning of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+
+		/* Free the string and then the node */
+		free(temp->str);
+		free(temp);
+	}
+}
