56a4b8bcfc829a41f462de826af56224689bd3e6
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2023-12-02 13:57:27 -0400

    bit

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -6,14 +6,14 @@
  * @n: The number to extract the bit from.
  * @index: The index of the bit to retrieve (starting from 0).
  *
- * Return: The value of the bit at the specified index, or -1 if an error occurs.
+ * Return: The value of the bit at
+ * the specified index, or -1 if an error occurs.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	/* Check if the index is greater than the number of bits in unsigned long int */
 	if (index >= sizeof(unsigned long int) * 8)
-		return -1;
+		return (-1);
 
 	/* Right shift n to the index position and check the least significant bit */
-	return (int)((n >> index) & 1);
+	return ((int)((n >> index) & 1));
 }
