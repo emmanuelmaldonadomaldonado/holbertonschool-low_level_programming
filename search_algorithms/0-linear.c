c564db082f74dca69125c9731aec0d9e35bd0069
emmanuelmaldonadomaldonado <7375@holbertonstudents.com>
2024-01-17 13:11:02 -0400

    linear

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -11,11 +11,13 @@
  * Return: Index of the first occurrence of the value, or -1 if not found
  */
 int linear_search(int *array, size_t size, int value)
-{
+{  
+    size_t i;
+
     if (array == NULL)
         return -1;
 
-    for (size_t i = 0; i < size; i++)
+    for (i = 0; i < size; i++)
     {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
@@ -23,5 +25,5 @@ int linear_search(int *array, size_t size, int value)
             return i;
     }
 
-    return -1; // Value not found
+    return -1;
 }
